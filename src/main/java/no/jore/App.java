c3b61f6f3ff2c921bcf84995d9aac824ca9cf652
JonasJore <jonas.jore@gmail.com>
2023-12-20 15:31:05 +0100

    made it possible to run jar files after building with maven. also removed dependency

--- src/main/java/no/jore/App.java
+++ src/main/java/no/jore/App.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 public class App {
   public static void main(String[] args) throws IOException {
-    System.out.println("Hello World!");
     MazeGame instance = new MazeGame();
 
     instance.gameLoop();
