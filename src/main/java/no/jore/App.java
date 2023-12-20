2e787f40fa781b877abb74f3379e2d9fb9398cdd
JonasJore <jonas.jore@gmail.com>
2023-12-20 15:33:55 +0100

    removed throws in main

--- src/main/java/no/jore/App.java
+++ src/main/java/no/jore/App.java
@@ -3,7 +3,7 @@ package no.jore;
 import java.io.IOException;
 
 public class App {
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     MazeGame instance = new MazeGame();
 
     instance.gameLoop();
